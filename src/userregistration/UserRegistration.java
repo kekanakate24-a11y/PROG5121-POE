140e1a83e0ab1b8126778783888c9aea82392ae6
kekanakate24-a11y <kekanakate24@gmail.com>
2026-04-11 15:28:42 +0200

    my third  commit new feature

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -8,6 +8,21 @@ public class UserRegistration {
     
     public static void main(String[] args) {
         
+        Scanner input = new Scanner(System.in);
+        LogIn login = new LogIn();
+        
+        System.out.println(" REGISTER NEW ACCOUNT ");
+        
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+        
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+        
+        System.out.print("Enter cell phone number (+27): ");
+        String cell = input.nextLine();
+        
+        String registrationResult = login.registerUser(username, password, cell);
     }
     
 }
