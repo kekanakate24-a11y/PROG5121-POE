29883e4f9315b0b687b73303377086a75af542ad
kekanakate24-a11y <kekanakate24@gmail.com>
2026-04-21 17:18:38 +0200

    Part 2, messages + new feature included 4th commit

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -50,11 +50,44 @@ public class UserRegistration {
     
     // quick chat message app
     if(loginStatus){
-        System.out.println("WELCOME TO QUICKCHAT")S;
+        System.out.println("WELCOME TO QUICKCHAT");
         System.out.print("How many messages do you want to print?");
         int maxMessages = input.nextInt();
         input.nextLine();
-    }
-    }
-  }
-}
+        
+        int messageCount = 0;
+        
+        while(true){
+            System.out.println("MAIN MENU");
+            System.out.println("Option 1) Send Messages");
+            System.out.println("Option 2) Show recently sent messages ");
+            System.out.println("Option 3) Quit");
+            int option = input.nextLine();
+            
+            switch (option){
+                
+                //option 1 send messages
+                case 1:
+                    if(messageCount >= maxMessages){
+                        System.out.println("You have reached your message limit");
+                        
+                        break;
+                    
+                    System.out.print("Enter recepient phone number (+27...)");
+                    String recepient = input.nextLine();
+                    
+                    if(!Message.checkRecepientCell(recepient)) {System.out.println("Invalid phone number.");
+                        break;
+                    }
+                    System.out.print("Enter message with maximum 250 characters: ");
+                    String text = input.nextLine();
+                    
+                    if(!checkMessageLength(text)){
+                       System.out.println("Message exceeds 250 characters.");
+                    break;
+                    }
+                    Messages newMsg = new Message(messageCount + 1, recepient, text);
+                    newMsg.messageMenu();
+                    
+ }      
+}}}}}
